dbe56f55a8f01b15651e48c9bba9e3eb1b839ec5
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-01-11 16:33:04 +0400

    Updates

---
+++ precache-manifest.1abc52c0836a08eefa12daaa004f9044.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f40db5321759a4ed854b6c87f1d216f",
+    "url": "/change-color/index.html"
+  },
+  {
+    "revision": "9375eb7dbc1663fb0371",
+    "url": "/change-color/static/css/main.0919c5af.chunk.css"
+  },
+  {
+    "revision": "82f644b6139bb55605c2",
+    "url": "/change-color/static/js/2.300c57f1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/change-color/static/js/2.300c57f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9375eb7dbc1663fb0371",
+    "url": "/change-color/static/js/main.4cc919dd.chunk.js"
+  },
+  {
+    "revision": "e364428806a1ee301be4",
+    "url": "/change-color/static/js/runtime-main.85aaad33.js"
+  }
+]);
