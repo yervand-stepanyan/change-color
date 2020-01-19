a4b3100736d0f4be015e13ecc95dd09fcbab52ec
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-01-19 10:36:40 +0400

    Add static variables

--- src/Components/ChangeColor/ChangeColor.js
+++ src/Components/ChangeColor/ChangeColor.js
@@ -8,28 +8,24 @@ export default class ChangeColor extends React.Component {
     super(props);
 
     this.state = {
-      textColorInput: 'color',
-      backgroundColorInput: 'backgroundColor',
-      colorLabel: 'Color :',
-      backgroundColorLabel: 'Background color :',
       color: '',
       backgroundColor: ''
     };
   }
+
+  static VARIABLES = {
+    textColorInput: 'color',
+    backgroundColorInput: 'backgroundColor',
+    colorLabel: 'Color :',
+    backgroundColorLabel: 'Background color :'
+  };
 
   setColorValues = colorData => {
     this.setState({ [colorData.inputType]: colorData.color });
   };
 
   render() {
-    const {
-      textColorInput,
-      backgroundColorInput,
-      colorLabel,
-      backgroundColorLabel,
-      color,
-      backgroundColor
-    } = this.state;
+    const { color, backgroundColor } = this.state;
 
     const styles = { color, backgroundColor };
 
@@ -41,20 +37,22 @@ export default class ChangeColor extends React.Component {
           </div>
           <div className="inputs">
             <div className="colorDiv">
-              <label htmlFor={textColorInput}>{colorLabel}</label>
+              <label htmlFor={ChangeColor.VARIABLES.textColorInput}>
+                {ChangeColor.VARIABLES.colorLabel}
+              </label>
               <Input
-                id={textColorInput}
-                inputType={textColorInput}
+                id={ChangeColor.VARIABLES.textColorInput}
+                inputType={ChangeColor.VARIABLES.textColorInput}
                 passColorData={this.setColorValues}
               />
             </div>
             <div className="backgroundColorDiv">
-              <label htmlFor={backgroundColorInput}>
-                {backgroundColorLabel}
+              <label htmlFor={ChangeColor.VARIABLES.backgroundColorInput}>
+                {ChangeColor.VARIABLES.backgroundColorLabel}
               </label>
               <Input
-                id={backgroundColorInput}
-                inputType={backgroundColorInput}
+                id={ChangeColor.VARIABLES.backgroundColorInput}
+                inputType={ChangeColor.VARIABLES.backgroundColorInput}
                 passColorData={this.setColorValues}
               />
             </div>
